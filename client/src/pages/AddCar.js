7036e8b3cbc5eec3cc21c741d1cb58522936c778
abdulmoiz zahid <a.choudry_19751@khi.iba.edu.pk>
2022-11-28 21:54:29 +0500

    main

--- client/src/pages/AddCar.js
+++ client/src/pages/AddCar.js
@@ -1,10 +1,40 @@
-import { Col , Row , Form , Input} from 'antd'
-import React from 'react'
+import { Col , Row , Form , Input, message} from 'antd'
+import React , {useState} from 'react'
 import { useDispatch , useSelector } from 'react-redux'
 import DefaultLayout from '../components/DefaultLayout'
 import Spinner from '../components/Spinner'
 import { addCar } from '../redux/actions/carsActions'
+import UploadImageToS3WithReactS3 from '../components/UploadImageToS3WithReactS3'
+import { uploadFile } from 'react-s3';
+
+const S3_BUCKET ='rentitwebapp';
+const REGION ='ap-south-1';
+const ACCESS_KEY ='AKIAYTMB5AL65CJHKM74';
+const SECRET_ACCESS_KEY ='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';
+
+const config = {
+    bucketName: S3_BUCKET,
+    region: REGION,
+    accessKeyId: ACCESS_KEY,
+    secretAccessKey: SECRET_ACCESS_KEY,
+}
+
+
 function AddCar() {
+    const [selectedFile, setSelectedFile] = useState(null);
+    const handleFileInput = (e) => {
+
+        handleUpload(e.target.files[0]);
+        
+    }
+
+    const handleUpload = async (file) => {
+        uploadFile(file, config)
+            .then(data => setSelectedFile(data.location))
+            .catch(err => console.error(err))
+
+        
+    }
 
     const dispatch = useDispatch()
     const {loading} = useSelector(state=>state.alertsReducer)
@@ -13,7 +43,13 @@ function AddCar() {
 
          values.bookedTimeSlots=[]
 
-         dispatch(addCar(values))
+         values.image=selectedFile
+         if(values.image!=null){
+            dispatch(addCar(values))
+         }else{
+            message.error("fill all details")
+         }
+         
          console.log(values)
     }
 
@@ -27,10 +63,7 @@ function AddCar() {
                            <hr />
                            <Form.Item name='name' label='Car name' rules={[{required: true}]}>
                                <Input/>
-                           </Form.Item>
-                           <Form.Item name='image' label='Image url' rules={[{required: true}]}>
-                               <Input/>
-                           </Form.Item>
+                           </Form.Item>                           
                            <Form.Item name='rentPerHour' label='Rent per hour' rules={[{required: true}]}>
                                <Input/>
                            </Form.Item>
@@ -40,7 +73,10 @@ function AddCar() {
                            <Form.Item name='fuelType' label='Fuel Type' rules={[{required: true}]}>
                                <Input/>
                            </Form.Item>
-
+                           <Form.Item  label='Add Image'>
+                           <input type="file" onChange={handleFileInput}/>
+                           {selectedFile}
+                           </Form.Item>
                            <div className='text-right'>
                            <button className='btn1'>ADD CAR</button>
                            </div>
--- server.js
+++ server.js
@@ -2,15 +2,15 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
 const dbConnection = require('./db')
+
 app.use(express.json())
 
 app.use('/api/cars/' , require('./routes/carsRoute'))
 app.use('/api/users/' , require('./routes/usersRoute'))
 app.use('/api/bookings/' , require('./routes/bookingsRoute'))
 
-
 const path = require('path')
-
+ 
 if(process.env.NODE_ENV==='production')
 {
 
